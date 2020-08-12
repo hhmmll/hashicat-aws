5aa27580e65fc66bd4fefd38cfcda3852d4b5cc3
Kensin Wu <hhmmll@gmail.com>
2020-08-12 08:38:10 +0000

    fix sg

--- sg.tf
+++ sg.tf
@@ -2,7 +2,13 @@ module "security-group" {
   source  = "hhmmll/security-group/aws"
   version = "3.13.0"
   # insert the 2 required variables here
-  name = "sg-from-module"
+  name = "${var.prefix}-security-group-from-module"
   vpc_id = "${aws_vpc.hashicat.id}"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
