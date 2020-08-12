80e455c40000ba8a126f795dd6bcd7f10d4ff042
Kensin Wu <hhmmll@gmail.com>
2020-08-12 08:39:55 +0000

    fix sg2

--- sg.tf
+++ sg.tf
@@ -4,11 +4,4 @@ module "security-group" {
   # insert the 2 required variables here
   name = "${var.prefix}-security-group-from-module"
   vpc_id = "${aws_vpc.hashicat.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
