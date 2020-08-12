da6178c1c358d401b16ba771a826c88f71feae77
Kensin Wu <hhmmll@gmail.com>
2020-08-12 07:47:26 +0000

    update my code 2

--- main.tf
+++ main.tf
@@ -114,6 +114,7 @@ resource "aws_eip_association" "hashicat" {
   allocation_id = aws_eip.hashicat.id
 }
 
+
 resource aws_instance "hashicat" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
